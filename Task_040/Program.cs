7e98b6f26b10dba0bc92cea4ed2a4fb7e9f917ff
stevgeniy <stepin_evgen@mail.ru>
2023-03-28 22:00:58 +0300

    Task_040

---
+++ Task_040/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
+Console.Clear();
+int A = GetNumberFromUser("Введите длинну 1 стороны: ", "Ошибка ввода!");
+int B = GetNumberFromUser("Введите длинну 2 стороны: ", "Ошибка ввода!");
+int C = GetNumberFromUser("Введите длинyу 3 стороны: ", "Ошибка ввода!");
+bool isTringle = IsTringle(A, B, C);
+Console.WriteLine(isTringle? "Да" : "Нет");
+//////////////////////////////////////////////////////////////////////////////////
+// Запрашивает у пользователя целые числа
+int GetNumberFromUser(string message, string errorMessage)
+{
+while(true)
+{
+Console.Write(message);
+bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+if (isCorrect && userNumber > 0)
+{
+return userNumber;
+}
+Console.WriteLine(errorMessage);
+}
+}
+
+bool IsTringle(int a, int b, int c)
+{
+    return(a + b > c && b + c > a && a + c > b);
+}
