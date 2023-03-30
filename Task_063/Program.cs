3080b612c8e31a97610a2ea14671b4fca7d925a8
stevgeniy <stepin_evgen@mail.ru>
2023-03-30 08:00:24 +0300

    Task_063

---
+++ Task_063/Program.cs
@@ -0,0 +1,32 @@
+﻿int a = GetNumberFromUser("Введите натуральное число N: ","Ошибка ввода");
+int b = GetNumberFromUser("Введите натуральное число M: ","Ошибка ввода");
+
+//string result = GetNumbersFromMToN(b,a);
+
+Console.WriteLine($"{b} -> {a} : {GetNumbersFromMToN(a,b)}");
+
+static string GetNumbersFromMToN(int fromNumber, int toNumber)
+{ 
+    if (fromNumber < toNumber)
+         return fromNumber + ", " + GetNumbersFromMToN(fromNumber + 1, toNumber);
+    if (fromNumber > toNumber)
+         return toNumber + ", " + GetNumbersFromMToN(toNumber + 1, fromNumber);
+    else   
+        return toNumber.ToString();            
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int usernamber);
+        if (isCorrect)
+        {
+            return usernamber;
+        }  
+        Console.WriteLine(errorMessage);
+
+    }
+    
+}
