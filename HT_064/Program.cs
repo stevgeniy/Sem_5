68eeb1445ce5be202f871a85135919e00ea37d6c
stevgeniy <stepin_evgen@mail.ru>
2023-04-04 22:52:43 +0300

    HT_064

---
+++ HT_064/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = GetNumberFromUser("Введите натуральное число N: ","Ошибка ввода");
+
+//string result = GetNumbersFromMToN(b,a);
+
+Console.WriteLine($"{a} -> 1 : {GetNumbersFromMToN(a)}");
+
+static string GetNumbersFromMToN(int fromNumber)
+{ 
+    if (fromNumber > 1)
+         return fromNumber + ", " + GetNumbersFromMToN(fromNumber - 1);
+    else   
+        return fromNumber.ToString();            
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int usernamber);
+        if (isCorrect)
+        {
+            return usernamber;
+        }  
+        Console.WriteLine(errorMessage);
+
+    }
+    
+}
